d6d1dd6b2634ef58c74fc88992364aab8150412f
yangfan789789 <1062208122@qq.com>
2019-11-16 20:45:32 +0800

    yangfan

---
+++ build/precache-manifest.659fbb72b02a2afdbff07ff97c470f35.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4659807f213357198f9dbf950693b03",
+    "url": "./index.html"
+  },
+  {
+    "revision": "baff1f949e8cb2e98223",
+    "url": "./static/css/2.6f60e7f3.chunk.css"
+  },
+  {
+    "revision": "2238a942e11553e48341",
+    "url": "./static/css/main.e28fc22d.chunk.css"
+  },
+  {
+    "revision": "baff1f949e8cb2e98223",
+    "url": "./static/js/2.88344ce7.chunk.js"
+  },
+  {
+    "revision": "2238a942e11553e48341",
+    "url": "./static/js/main.43f2bc1e.chunk.js"
+  },
+  {
+    "revision": "ed399c76eafa58f3821e",
+    "url": "./static/js/runtime-main.13bb51ee.js"
+  },
+  {
+    "revision": "26b84fedbf5e6813812e74eb030c5f81",
+    "url": "./static/media/住吧首页_02.26b84fed.jpg"
+  },
+  {
+    "revision": "ba42e22294e5d77386ba68962d7aef78",
+    "url": "./static/media/住吧首页_03.ba42e222.jpg"
+  },
+  {
+    "revision": "74c3c9d522b0401ccdfa91668c86e88d",
+    "url": "./static/media/住吧首页_06.74c3c9d5.jpg"
+  },
+  {
+    "revision": "40beec2583a7ca90178f9faa1f7b8bbb",
+    "url": "./static/media/住吧首页_08.40beec25.jpg"
+  },
+  {
+    "revision": "4b6d56fac7999d324947c9d59d8f1882",
+    "url": "./static/media/住吧首页_13.4b6d56fa.jpg"
+  },
+  {
+    "revision": "cbbd4f3bede7cbbc491b01bcfa6d8449",
+    "url": "./static/media/商城_03.cbbd4f3b.jpg"
+  },
+  {
+    "revision": "2a1f219c2534f38f77178ae60c4748fd",
+    "url": "./static/media/商城_05.2a1f219c.jpg"
+  },
+  {
+    "revision": "2be6f037dbd483308e7015363dc76239",
+    "url": "./static/media/灵感_03.2be6f037.jpg"
+  },
+  {
+    "revision": "5b62e84f02716a41f5379d099d114e08",
+    "url": "./static/media/灵感_05.5b62e84f.jpg"
+  },
+  {
+    "revision": "0c360fb5b39bb6c5239884890c966a90",
+    "url": "./static/media/灵感_09.0c360fb5.jpg"
+  },
+  {
+    "revision": "e94a78acaaa4e4ab379e1f1a7ea5d195",
+    "url": "./static/media/灵感_10.e94a78ac.jpg"
+  }
+]);
